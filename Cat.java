d569974b099597c10697429e3cb502fb367d6a5f
Aleksey <42946400+panzershracker@users.noreply.github.com>
2021-09-03 12:44:18 +0300

    ДЗ к уроку 7.
    В принципе сделано, но нужно переделать защиту тарелки от отрицательного значения правильно. На данный момент ограничитель на уровне класса Cat.

---
+++ Cat.java
@@ -0,0 +1,39 @@
+package les_7;
+
+public class Cat {
+    private final String name;
+    private final int appetite;
+    private boolean bellyful = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    protected String getName() {
+        return this.name;
+    }
+
+    protected int getAppetite() {
+        return this.appetite;
+    }
+
+    protected boolean getBellyful() {return this.bellyful;}
+
+    public void eat (Plate plate) {
+        if (appetite > plate.getFoodQuantity()) {
+            System.out.println("В тарелке меньше еды чем нужно " + name + "у и он НЕ поел!\n");
+        }else {plate.decreaseFood(appetite);
+                bellyful = true;
+                System.out.println(name + " Поел\n");};
+    }
+
+    public void status() {
+        System.out.printf("Статус:%n" +
+                "Имя = %s%n " +
+                "Аппетит = %s%n" +
+                "Сытый = %s%n%n", name, appetite, bellyful);
+    }
+
+
+}
---
+++ Plate.java
@@ -0,0 +1,28 @@
+package les_7;
+
+public class Plate {
+    private static int foodQuantity;
+
+    public Plate(int foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+
+    public int getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public void decreaseFood(int n) {
+        foodQuantity -= n;
+    }
+
+    public void info() {
+        System.out.println("Кол-во еды в тарелке = " + foodQuantity);
+    }
+
+    public void fillPlate(int fillQuantity) {
+        System.out.println("Добавим еду в тарелку");
+        System.out.println("Было в тарелке = " + foodQuantity);
+        foodQuantity += fillQuantity;
+        System.out.println("Стало в тарелке = " + foodQuantity + "\n\n");
+    }
+}
