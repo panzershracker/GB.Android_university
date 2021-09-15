47a1986562fc55c27decaa8b44e36d6f782d2e37
Aleksey <42946400+panzershracker@users.noreply.github.com>
2021-09-15 19:48:20 +0300

    ДЗ 10

---
+++ PhoneBook.java
@@ -0,0 +1,36 @@
+package les_10;
+
+import java.util.*;
+
+public class PhoneBook {
+
+    String name;
+    int phone;
+
+    Map<Integer,String> phonebook = new HashMap<>();
+
+
+    public void add(String name, Integer phone, boolean print){
+        phonebook.put(phone, name);
+        if (print){
+            System.out.printf("Name %s, phone %s was added to phonebook %n", name, phone);
+        }
+    }
+
+    public void get(String contactName) {
+        for (Integer phone : phonebook.keySet()) {
+            name = phonebook.get(phone);
+            if (contactName == name) {
+                System.out.printf("Name = %s, phone number = %s %n", name, phone);
+            }
+        }
+    }
+
+    public void printBook() {
+        for (Integer phone : phonebook.keySet()) {
+            name = phonebook.get(phone);
+            System.out.printf("Name = %s, number = %s %n", name, phone);
+        }
+//        System.out.println(phonebook);
+    }
+}
